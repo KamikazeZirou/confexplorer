b5ff8b8f3e117770ac8cc8847d645a85e42a37be
kamikaze.zirou <kamikaze.zirou@gmail.com>
2020-07-29 00:16:57 +0900

    chapter3 A first static page with React

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,11 +6,24 @@ group = "org.example"
 version = "1.0-SNAPSHOT"
 
 repositories {
+    maven("https://kotlin.bintray.com/kotlin-js-wrappers/")
     mavenCentral()
+    jcenter()
 }
 
 dependencies {
     implementation(kotlin("stdlib-js"))
+
+    // React
+    implementation(Deps.Kotlin.React)
+    implementation(Deps.Kotlin.ReactDom)
+    implementation(npm(Deps.Npm.React, Versions.npmReact))
+    implementation(npm(Deps.Npm.ReactDom, Versions.npmReact))
+
+    //Kotlin Styled
+    implementation(Deps.Kotlin.Styled)
+    implementation(npm(Deps.Npm.StyledComponents))
+    implementation(npm(Deps.Npm.InlineStylePrefixer))
 }
 
 kotlin.target.browser { }
--- buildSrc/src/main/java/Deps.kt
+++ buildSrc/src/main/java/Deps.kt
@@ -1,8 +1,21 @@
 object Versions {
     const val kotlin = "1.3.72"
+    const val npmReact = "16.13.1"
 }
 
 object Deps {
+    object Kotlin {
+        const val React = "org.jetbrains:kotlin-react:16.13.1-pre.105-kotlin-${Versions.kotlin}"
+        const val ReactDom = "org.jetbrains:kotlin-react-dom:16.13.1-pre.105-kotlin-${Versions.kotlin}"
+        const val Styled = "org.jetbrains:kotlin-styled:1.0.0-pre.110-kotlin-${Versions.kotlin}"
+    }
+
+    object Npm {
+        const val React = "react"
+        const val ReactDom = "react-dom"
+        const val StyledComponents = "styled-components"
+        const val InlineStylePrefixer = "inline-style-prefixer"
+    }
 }
 
 object TestDeps {
