ee1451faf86baa9d7f3369b4807ab87e26608b51
kamikaze.zirou <kamikaze.zirou@gmail.com>
2020-07-29 00:51:24 +0900

    chapter4 Making It React: Our first Component

---
+++ src/main/kotlin/component/VideoList.kt
@@ -0,0 +1,42 @@
+package component
+
+import kotlinx.html.js.onClickFunction
+import model.Video
+import react.*
+import react.dom.p
+import kotlin.browser.window
+
+class VideoList: RComponent<VideoListProps, VideoListState>() {
+    override fun RBuilder.render() {
+        props.videos.forEach { video ->
+            p {
+                key = video.id.toString()
+                attrs {
+                    onClickFunction = {
+                        setState {
+                            selectedVideo = video
+                        }
+                    }
+                }
+                if (video == state.selectedVideo) {
+                    +"▶ "
+                }
+                +"${video.speaker}: ${video.title}"
+            }
+        }
+    }
+}
+
+external interface VideoListProps: RProps {
+    var videos: List<Video>
+}
+
+external interface VideoListState: RState {
+    var selectedVideo: Video?
+}
+
+fun RBuilder.videoList(handler: VideoListProps.() -> Unit): ReactElement {
+    return child(VideoList::class) {
+        this.attrs(handler)
+    }
+}
